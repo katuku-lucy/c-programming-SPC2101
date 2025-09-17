b465720452a8db0e3238a28ebfb118c186f2f3ec
katuku-lucy <neemalucie129@gmail.com>
2025-09-17 14:59:25 +0300

    Add files via upload

---
+++ KATUKU'S PROGRAMMING .c
@@ -0,0 +1,22 @@
+//simple c program 
+/*
+Name :Katuku muthami
+Reg :CT100/G/26212/25
+Description.: to display all units for this semester 
+*/
+
+#include<stdio.h>
+
+//main function
+int main() {
+    printf("all units in this semester:\n"); 
+    printf("mathematics for science\n" );
+    printf ("introduction to computer application\n");
+    printf("analogue electronics\n" );
+    printf("introduction to programming and algorithms\n");
+    printf("essentials of accounting\n");
+    printf("computer systems and organization\n");
+    printf("communication and literary skills\n");
+    printf("fundamentals of ICT\n");
+    return 0;
+}
---
+++ Question 2 Coding2.c
@@ -0,0 +1,24 @@
+//simple c program 
+/*
+Name:Katuku Muthami 
+
+Reg :CT100/G/26212/25
+*/
+#include<stdio.h>
+
+int main(){
+    float height, weight;
+    int phone;
+    
+    printf("Enter your height:");
+    scanf("%f",&height);
+    printf("Enter your phone number:");
+    scanf("%d",&phone);
+    printf("Enter your weight:");
+    scanf("%f",&weight);
+    
+    printf("your height is %.2f\n",height);
+    printf("your phone number is %.d\n",phone);
+    printf("your weight is %.2f\n",weight);
+    return 0;
+}
