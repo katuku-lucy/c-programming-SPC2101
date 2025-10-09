c5b8289e92e891ad503dee1179f121136aeccb5a
katuku-lucy <neemalucie129@gmail.com>
2025-10-09 19:25:30 +0300

    Add files via upload

---
+++ loopofnumbersindescendingorder.c
@@ -0,0 +1,18 @@
+/*
+Name : Katuku Muthami 
+Reg : CT100/G/26212/25
+Description: a loop program 
+c program to print 100-50
+*/
+
+#include<stdio.h>
+
+int main(){
+    //for(start; stop ; step){}
+    int i;
+    for (i=100; i>=50; i--){
+    printf("%d \n", i);
+    }
+    
+    return 0;
+}
